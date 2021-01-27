bad915eb34113575cfbb002190c2dd548b678c30
tsamantanis <philip.tsamantanis@icloud.com>
2021-01-27 15:20:27 -0800

    Specify port in .env file

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () =>
-    console.log('App listening on port 3000!'),
+app.listen(process.env.PORT, () =>
+    console.log(`App listening on port ${process.env.PORT}!`),
 );
