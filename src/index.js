490b0572cab790df5a4be1f32b7698eed780dd51
tsamantanis <philip.tsamantanis@icloud.com>
2021-01-27 15:26:43 -0800

    Simple CRUD for users

--- src/index.js
+++ src/index.js
@@ -26,6 +26,22 @@ app.delete('/', (req, res) => {
     return res.send('Received a DELETE HTTP method');
 });
 
+app.get('/users', (req, res) => {
+    return res.send('GET HTTP method on user resource');
+});
+
+app.post('/users', (req, res) => {
+    return res.send('POST HTTP method on user resource');
+});
+
+app.put('/users', (req, res) => {
+    return res.send('PUT HTTP method on user resource');
+});
+
+app.delete('/users', (req, res) => {
+    return res.send('DELETE HTTP method on user resource');
+});
+
 app.listen(process.env.PORT, () =>
     console.log(`App listening on port ${process.env.PORT}!`),
 );
