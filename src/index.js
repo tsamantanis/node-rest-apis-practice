33885f90194fa2187c67185bc170028c7d1b26cf
tsamantanis <philip.tsamantanis@icloud.com>
2021-01-27 15:19:35 -0800

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import 'dotenv/config';
-
+import cors from 'cors';
 import express from 'express';
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
